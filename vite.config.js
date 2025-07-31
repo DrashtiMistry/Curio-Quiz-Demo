24e34fcc03d73d39d24a8d2e57c8f794ec80d8de
darshanp001 <145213221+darshanp001@users.noreply.github.com>
2025-08-01 02:26:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-<<<<<<< HEAD
-  server: {
-    port: 3000,
-    host: true,
-  }, 
-=======
->>>>>>> 59652221922af32677c78ece6bf8c9f7994bb896
+ 
 })
