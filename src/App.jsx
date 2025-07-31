3660738181ccc44bf90f0aa9c5f27fd9b9f00783
darshanp001 <145213221+darshanp001@users.noreply.github.com>
2025-08-01 02:14:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import TestTypePage from './pages/TestTypePage';
-import QuizForm from './components/QuizForm';
-import AnalysisPage from './pages/AnalysisPage';
-import Layout from './Layouts/Layout';
-import ExamSelectionPage from './pages/ExamSelectionPage';
-import ChapterSelectionPage from './pages/ChapterSelectionPage';
 
-function App() {
-  return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/exams" element={<ExamSelectionPage />} />
-          <Route path="/test-type" element={<TestTypePage />} />
-          <Route path="/chapters" element={<ChapterSelectionPage />} />
-          <Route path="/quiz" element={<QuizForm />} />
-          <Route path="/analysis" element={<AnalysisPage />} />
-        </Routes>
-      </Layout>
-    </Router>
-  );
-}
-
-export default App;
-=======
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
@@ -61,4 +32,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 59652221922af32677c78ece6bf8c9f7994bb896
